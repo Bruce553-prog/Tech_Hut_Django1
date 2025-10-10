f97612036a207c7010dc5d37c7ba867b5479506c
bruce wyllis <brucewyllis01@gmail.com>
2025-10-10 15:49:40 +0300

    disabling admins from editing users informations and borrowers data  while retaining the ability to add and delete users

--- sme_credit/admin.py
+++ sme_credit/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 from .models import BorrowerApplication
-admin.site.register(BorrowerApplication)
+from .models import BorrowerApplication
+from .models import BorrowerApplication
+
+@admin.register(BorrowerApplication)
+class BorrowerApplicationAdmin(admin.ModelAdmin):
+    def has_change_permission(self, request, obj=None):
+        return False
+
+
